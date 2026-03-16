c94d7145a3b14dfebce3dc3f1e91052897f0c909
kalbashi09 <pauljode1234@gmail.com>
2026-03-16 10:15:56 +0800

    first commit

---
+++ config.js
@@ -0,0 +1,5 @@
+// This file stays in your frontend folder
+const HEALERTSYS_CONFIG = {
+  apiKey: "h43dsHfjKS956032b8a9e5c1f0e4b",
+  apiURL: "https://backend-9lv5.onrender.com/api/live-heat-history",
+};
